1561a38e63c53ad5ab972ccd433162ed64249336
matiasnaakka <matias5230@gmail.com>
2022-04-22 09:27:54 +0300

    toimiva versio arberin koodi

--- app/src/main/java/com/example/fitnessmate/MainActivity.java
+++ app/src/main/java/com/example/fitnessmate/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.example.fitnessmate.DisplayMessageActivity;
+import com.example.fitnessmate.R;
 
 public class MainActivity extends AppCompatActivity {
-  public static final String Name_TEXT = "com.example.myfirstapp.MESSAGE";
-public static final String AGE_TEXT = "com.example.myfirstapp.AGE";
-  
+    public static final String Name_TEXT = "com.example.myfirstapp.MESSAGE";
+    public static final String AGE_TEXT = "com.example.myfirstapp.AGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,17 +21,17 @@ public static final String AGE_TEXT = "com.example.myfirstapp.AGE";
     }
 
 
+    /** Called when the user taps the Send button */
     public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayResultActivity.class);
-        Bundle nameAndAge = new Bundle();
-        EditText nameText = (EditText) findViewById(R.id.nameText);
-        EditText ageText = (EditText) findViewById(R.id.ageText);
-        String nameMessage = nameText.getText().toString();
-        String ageMessage = ageText.getText().toString();
-        nameAndAge.putString("NAME_MESSAGE", nameMessage);
-        nameAndAge.putString("AGE_MESSAGE", ageMessage);
-        intent.putExtras(nameAndAge);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.Name);
+        EditText editAge = (EditText) findViewById(R.id.Age);
+        String message = editText.getText().toString();
+        String ageMessage = editAge.getText().toString();
+        intent.putExtra(Name_TEXT, "Hello " + message +  ", you are " + ageMessage + " years old");
         startActivity(intent);
 
     }
+
+
 }
