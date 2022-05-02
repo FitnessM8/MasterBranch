c3ef24e6526d1785d2971600829408af76686f47
matiasnaakka <matias5230@gmail.com>
2022-05-02 18:57:48 +0300

    Arber's changes

--- app/src/main/java/com/example/fitnessmate/DisplayMessageActivity.java
+++ app/src/main/java/com/example/fitnessmate/DisplayMessageActivity.java
@@ -8,18 +8,27 @@ import static com.example.fitnessmate.MainActivity.BMI_RESULT_STRING;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.fitnessmate.R;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
+    Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_display_message);
-
 
         Bundle extras = getIntent().getExtras();
         String bmiNumber = extras.getString(BMI_RESULT_Number);
@@ -29,4 +38,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.bmi_result)).setText(bmiNumber);
         ((TextView) findViewById(R.id.bmi_result_number)).setText(bmiString);
     }
+
+    public void tipsURL(View view) {
+        Intent openTips = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.who.int/philippines/news/feature-stories/detail/20-health-tips-for-2020"));
+        startActivity(openTips);
+    }
+
+    public void onClick(View view) {
+        Intent backIntent = new Intent(DisplayMessageActivity.this, MainActivity.class);
+        startActivity(backIntent);
+    };
 }
