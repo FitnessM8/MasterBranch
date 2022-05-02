1458d67e5a3154334a04299a3d50c101ce51e48f
zarber <zarberleo@gmail.com>
2022-05-02 17:37:54 +0300

    Added some functionalities to buttons and made the result layout little bit more beautiful. Also added some more information in calculator and made some fixes on how the layout shows.

---
+++ app/src/main/java/com/example/fitnessmate/CalculateButton.java
@@ -0,0 +1,55 @@
+package com.example.fitnessmate;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import javax.xml.transform.Result;
+
+public class CalculateButton extends AppCompatActivity {
+
+    double bmi = 0;
+    double valueheight = 0;
+    double valueweight = 0;
+    double valueheightmeters;
+    private static String bmiString;
+    private static String bmiResult;
+
+
+    public void onClick(View view, TextView weightText, TextView heightText) {
+
+        valueheight = Double.parseDouble(heightText.getText().toString());
+        valueweight = Double.parseDouble(weightText.getText().toString());
+
+        valueheightmeters = valueheight / 100; // Converting to meters.
+
+        bmi = (valueweight / (valueheightmeters * valueheightmeters));
+        bmiString = String.format("%.1f" , bmi);
+
+        Log.d("Tag", String.valueOf(valueheightmeters));
+
+        if (bmi >= 30) {
+            Log.d("Lihavuus", String.valueOf(bmi));
+            bmiResult = "Lihavuus (normaali painoindeksi: 18.5 - 25)! Paras tapa pudottaa painoa on syömällä terveellisesta ja urheilulla. Vinkkejä saat painamalla alla olevasta napista.";
+        } else if (bmi >= 25) {
+            Log.d("Lievä lihavuus", String.valueOf(bmi));
+            bmiResult = "Lievä lihavuus (normaali painoindeksi: 18.5 - 25)! Vinkkejä laihduttamiseen saat alla olevasta napista.";
+        } else if (bmi >= 18.5) {
+            Log.d("Normaali paino", String.valueOf(bmi));
+            bmiResult = "Painoindeksisi on normaali! Lisää vinkkejä terveellisiin elämäntapoihin saat painamalla alla olevasta napista.";
+        } else {
+            Log.d("Alipaino", String.valueOf(bmi));
+            bmiResult = "Olet alipainonen (normaali painoindeksi: 18.5 - 25)! Muista syödä enemmän. Hyviä vinkkejä saat alla olevesta napista";
+        }
+
+    }
+    public String haeBmi() {
+        return this.bmiString;
+    }
+
+    public String haeLuokitus() {
+        return this.bmiResult;
+    }
+}
---
+++ app/src/main/java/com/example/fitnessmate/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.fitnessmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String BMI_RESULT_STRING = "bmiResultString";
+    public static final String BMI_RESULT_Number = "bmiResultNumber";
+
+    public SeekBar ageBar;
+    public SeekBar weightBar;
+    public SeekBar heightBar;
+    public static TextView ageText;
+    public static TextView weightText;
+    public static TextView heightText;
+    Button button;
+    ImageView male, female;
+    String typeofuser = "0";
+
+    CalculateButton bmiCalculateButton = new CalculateButton();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+//The application will display without Title
+
+        ageBar = (SeekBar) findViewById(R.id.Age_SeekBar);
+        weightBar = (SeekBar) findViewById(R.id.Weight_SeekBar);
+        heightBar = (SeekBar) findViewById(R.id.height_SeekBar);
+        ageText = (TextView) findViewById(R.id.Age_Number);
+        weightText = (TextView) findViewById(R.id.Weight_Number);
+        heightText = (TextView) findViewById(R.id.height_Number2);
+        button = (Button) findViewById(R.id.Calculate_Button);
+        weightBar.setMax(300);
+        heightBar.setMax(220);
+
+        male = findViewById(R.id.Male);
+        female = findViewById(R.id.Female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                male.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.genderselected));
+                female.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.gendernotselected));
+                typeofuser = "Memale";
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                female.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.genderselected));
+                male.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.gendernotselected));
+                typeofuser = "Female";
+                    }
+                });
+
+
+        BarListener.barListener(ageBar, ageText);
+        BarListener.barListener(weightBar, weightText);
+        BarListener.barListener(heightBar, heightText);
+
+
+//lähetetään tekstikenttien arvot calculateButton classiin
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bmiCalculateButton.onClick(view, weightText, heightText);
+
+                String bmi = bmiCalculateButton.haeBmi();
+                String bmiLuokka = bmiCalculateButton.haeLuokitus();
+
+                Intent intent = new Intent(MainActivity.this, DisplayMessageActivity.class);
+                intent.putExtra(BMI_RESULT_Number, bmi);
+                intent.putExtra(BMI_RESULT_STRING, bmiLuokka);
+                startActivity(intent);
+            }
+        });
+    }
+}
